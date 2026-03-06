50d4e9cda47452d226f7e166d12de1ae866ff873
1vanK <ivan.kozyrev@edunord.se>
2026-03-06 10:23:34 +0100

    3

--- FyllingData.sql
+++ FyllingData.sql
@@ -1,15 +1,15 @@
--- Добавляем студентов
+-- 
 INSERT INTO UserSchema.Students (FirstName, LastName)
 VALUES ('Tom', 'Hanks'), ('Leonardo', 'DiCaprio'), ('Denzel', 'Washington'), 
        ('Scarlett', 'Johansson'), ('Brad', 'Pitt'), ('Natalie', 'Portman');
 
--- Добавляем курсы
+--
 INSERT INTO UserSchema.Courses (CourseName)
 VALUES ('Cybersecurity Essentials'), ('Database Design and SQL'), 
        ('Introduction to Programming'), ('Network Fundamentals'), 
        ('Web Development Basics');
 
--- Добавляем записи об обучении (согласно второй картинке)
+-- 
 INSERT INTO UserSchema.Enrollments (StudentId, CourseId, Started, Ended)
 VALUES 
 (4, 1, '2025-05-02', '2025-06-28'), -- Johansson на Cybersecurity
